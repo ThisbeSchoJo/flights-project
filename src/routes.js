cb61a23976e86915b099a3507fe667e77e2c3af0
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-06 14:34:25 -0500

    added routing logic to render App component on '/' path

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+//Will be a data file that contains routes (so doesn't need to be in components folder)
+import App from "./components/App";
+
+const routes = [
+    {
+        path:"/",
+        element: <App />
+    }
+]
+
+export default routes;
