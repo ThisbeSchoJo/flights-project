797f4611e37a9950b84867c552124ca35fbfef9f
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-06 14:47:41 -0500

    added NavBar component which contains <NavLink> logic to be able to navigate to different paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 // import '../App.css';
 import { Outlet } from "react-router-dom"
 import Header from "./Header";
+import NavBar from "./NavBar";
 
 
 function App() {
   return (
     <div className="app">
+      <NavBar />
       <Header />
       <Outlet />
     </div>
