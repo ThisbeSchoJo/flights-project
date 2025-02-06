98dc3e9ef9761f1faf917f7abefff8faaaa49b5e
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-06 14:36:47 -0500

    added errorElement

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 //Will be a data file that contains routes (so doesn't need to be in components folder)
 import App from "./components/App";
+import ErrorPage from "./components/ErrorPage";
 
 const routes = [
     {
         path:"/",
         element: <App />
+        errorElement: <ErrorPage />
     }
 ]
 
